f8428adb1ce982a1eb96487b03482a0bfa8ee97b
fadhilmuhamaadd <121470307+fadhilmuhamaadd@users.noreply.github.com>
2024-07-17 10:32:08 +0700

    Update form.blade.php

--- resources/views/studi_kelayakan/form.blade.php
+++ resources/views/studi_kelayakan/form.blade.php
@@ -4,6 +4,7 @@
             @csrf
             @method('post')
 
+            {{-- op --}}
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
